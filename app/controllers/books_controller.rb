76cb33a945d1fbb8e473ff340ce02a5116e03b5d
yukito-0908 <tourentiancun@gmail.com>
2021-02-07 15:43:02 +0000

    [finish] 3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,52 @@
 class BooksController < ApplicationController
   def index
-
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
-    @book = Book.find(params[])
+    @book = Book.find(params[:id])
   end
 
   def new
      # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
-    @book = Book.new
-     @books = Book.all
   end
 
   def edit
+    @book = Book.find(params[:id])
+    @books = Book.all
   end
 
   def books
   end
 
   def create
-    book = Book.new(list_params)
-    book.save
-    redirect_to '/books'
+      @books = Book.all
+      @book = Book.new(list_params)
+      if @book.save
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to book_path(@book.id)
+      else
+        @books = Book.all
+        render 'index'
+      end
+  end
+
+  def update
+    @books = Book.all
+    @book = Book.find(params[:id])
+    if @book.update(list_params)
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to book_path(@book.id)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 root :to => 'books#books'
-get 'books' => 'books#new'
 get 'books' => 'books#index'
 post 'books' => 'books#create'
-get 'books/:id' => 'books#show' ,as: 'book'
+get 'books/:id' => 'books#show',as: 'book'
+get 'books/:id/edit' => 'books#edit', as: 'edit_books'
+patch 'books/:id' => 'books#update', as: 'update_books'
+delete 'books/:id' => 'books#destroy', as: 'destroy_books'
 end
